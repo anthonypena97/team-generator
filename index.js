d42ae5fddf7793e7096325bd30fbbdfb3f2dca87
Anthony Pena <apena5@alumni.jh.edu>
2021-09-07 16:15:32 -0400

    added mock data and application testing logic

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const Generator = require('./lib/Generator');
+const { Generator, GeneratorTest } = require('./lib/Generator');
 
 // START QUESTIONS FOR PAGE GENERATION
-new Generator().initializeQuestions();
+// new Generator().initializeQuestions();
+
+// TEST LOGIC - UNCOMMENT TO TEST
+new GeneratorTest().initializeQuestions();
