da9da4235683df151fccc8df0aa698baffb1d900
Anthony Pena <apena5@alumni.jh.edu>
2021-09-08 15:46:23 -0400

    finished all prompts using mock data

--- src/MockData.js
+++ src/MockData.js
@@ -1,28 +1,22 @@
-const managerMock = [
-    {
-        manager_name: 'Toto',
-        manager_id: '8490234',
-        manager_email: 'toto@email.com',
-        manager_office: '89403289043'
-    }
-];
+const managerMock = {
+    manager_name: 'Toto',
+    manager_id: '8490234',
+    manager_email: 'toto@email.com',
+    manager_office: '89403289043'
+};
 
-const engineerMock = [
-    {
-        engineer_name: 'Sandra',
-        engineer_id: '87897',
-        engineer_email: 'Sandra@email.com',
-        engineer_github: 'sandraGitHub'
-    }
-];
+const engineerMock = {
+    engineer_name: 'Sandra',
+    engineer_id: '87897',
+    engineer_email: 'Sandra@email.com',
+    engineer_github: 'sandraGitHub'
+};
 
-const internMock = [
-    {
-        intern_name: 'Alex',
-        intern_id: '787970',
-        intern_email: 'Alex@email.com',
-        intern_school: 'The University'
-    }
-];
+const internMock = {
+    intern_name: 'Alex',
+    intern_id: '787970',
+    intern_email: 'Alex@email.com',
+    intern_school: 'The University'
+};
 
 module.exports = { managerMock, engineerMock, internMock };
