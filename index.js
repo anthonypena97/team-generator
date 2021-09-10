5e5b3a560a475abb22eafb4e3d18521c07a0c813
Anthony Pena <apena5@alumni.jh.edu>
2021-09-09 20:23:26 -0400

    suspended

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Generator, GeneratorTest } = require('./lib/Generator');
 
 // START QUESTIONS FOR PAGE GENERATION
-// new Generator().initializeQuestions();
+new Generator().initializeQuestions();
 
 // TEST LOGIC - UNCOMMENT TO TEST
-new GeneratorTest().initializeQuestions();
+// new GeneratorTest().initializeQuestions();
